21b0507fa7e101beed2f11178458e9e6a5c5e1ca
סיאור מאיר <siorm2310@gmail.com>
2020-09-27 13:09:15 +0300

    Initial commit: Displaying entries works

---
+++ encyclopedia/views.py
@@ -0,0 +1,22 @@
+from django.http import request
+from django.shortcuts import render
+
+from . import util
+from markdown2 import Markdown
+
+MarkdownConverter = Markdown()
+
+def index(request):
+    return render(request, "encyclopedia/index.html", {
+        "entries": util.list_entries()
+    })
+
+def entry_page(request,entry):
+    try:
+        content = MarkdownConverter.convert(util.get_entry(entry)) 
+        return render(request, "encyclopedia/entry.html",{
+            "Entry_name": entry,
+            "content":content
+        })
+    except FileNotFoundError:
+        return render(request,"encyclopedia/error.html",{"error": 404})
